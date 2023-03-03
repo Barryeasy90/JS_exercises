e07e7bc82a649ed625250e9bba887de53441ef23
M.Barry <107761291+Barryeasy90@users.noreply.github.com>
2023-03-03 10:14:39 +0000

    Add files via upload

---
+++ javaScript exercise-week3.js
@@ -0,0 +1,45 @@
+// function getTruthness(value){
+//  if (value) {
+//  return "Thuthy";
+//  } else {return "Falsy";}
+// }
+// getTruthness("any")
+// console.log(getDay(0) === “Monday is businees day”);
+// console.log(getDay(3) === “Thursday is business day”);
+// console.log(getDay(6) === “Sunday is weekend”);
+// console.log(getDay(7) === “Uh-oh, the week comprises of just 7 days”);
+// function getDay(index) {
+// }
+
+// let allDayOfweek = ["Monday", "Tuesday","Wednesdat","Thursday","Fridat", "Saturday","Sunday"]
+// function getDay(index){
+//     if (index >= 0 && index < 5){
+//         return  '${allDayOfweek[index]} is business day';
+//     }
+//     else if (index < 7){
+//         return "${allDayOfweek[index]} is weekend ";
+//     }
+//     else {
+//      return ("Uh-oh, the week comprises of just 7 days")
+// }
+// }
+
+// getDay(6);
+
+// let allDayOfweek = [];
+// for ( let i=0; i<7; i++){
+//     console.log("Enter day of the week")
+//     allDayOfweek
+// }
+// function getIndex(array, item) {
+//     for ( let i=0; i < array.length-1; i++){
+//         if (array[i] === item){
+//             number = i;
+//         }
+//      }
+//      return number
+// }
+// const arr = [1, 3, 5, 7, 9, 100];
+// console.log(getIndex(arr, 5)); // 2
+// console.log(getIndex(arr, 100)); // 5
+// console.log(getIndex(arr, 1)); // 0
---
+++ js-week1.js
@@ -0,0 +1,27 @@
+/*const numberOfTraineers=45;
+const numberOfMentors=5;
+const totalNumberOfParticipant = numberOfTraineers + numberOfMentors;
+const message="Total number of participant is"
+console.log(message)
+console.log(totalNumberOfParticipant)
+let greeting="Hello World"
+console.log(greeting)
+
+function formatPenceToPounds(amount){
+    
+    let convert = amount/100;
+    return `£ ${convert}`;
+
+}
+const  exchange = formatPenceToPounds(300);
+console.log(exchange);*/
+function increaseByHalf(number){
+
+    return number + number/2;
+
+}
+/*let numberUp = increaseByHalf(40);
+console.log(numberUp);*/
+const pocketMoney = 413;
+let numberUp = increaseByHalf(pocketMoney);
+console.log(numberUp);
